b234537ca3ad9dcdb4b1b938cde132868fb3a1fd
Ambrus Dániel <daniel.ambrus92@gmail.com>
2023-01-14 14:09:39 +0100

    gombnyomasra

---
+++ app.js
@@ -0,0 +1,26 @@
+const btn = document.getElementById("btn");
+const result = document.querySelector(".result");
+
+btn.addEventListener("click", async () =>
+
+{
+    const res = await fetch("cars.json");
+    const data = await res.json();
+
+    console.log(data);
+
+    let output = "";
+    data.map(car =>{
+
+        output +=`
+        <div class="car">
+            <h2>Márka: ${car.Márka}</2>
+            <h3>Típus: ${car.Típus}</3>
+            <h3>Évjárat: ${car.Évjárat}</3>
+        </div>
+        `
+
+    })
+   result.innerHTML=output;
+}
+)
